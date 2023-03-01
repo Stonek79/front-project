2b8fae7cdfdcd72cefde135f83a696321f0f6215
Stonek79 <amit-yurist@mail.ru>
2023-03-01 15:52:53 +0300

    [feat] add husky, fix github pipeline

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
         '../../src/**/*.stories.@(js|jsx|ts|tsx)',
     ],
     addons: [
+        '@storybook/addon-actions',
         '@storybook/addon-links',
         '@storybook/addon-essentials',
         '@storybook/addon-interactions',
