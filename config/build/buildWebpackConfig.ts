694e4f5dd14a511f9b9e447124015ce93f0b0139
stonek <amit-yurist@mail.ru>
2023-05-21 14:38:10 +0300

    disabled devtools in buildWebpackConfig, add to build command

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,14 +1,14 @@
-import webpack from 'webpack';
-import { BuildOptions } from './types/config';
-import { buildPlugins } from './buildPlugins';
-import { buildLoaders } from './buildLoaders';
-import { buildResolvers } from './buildResolvers';
-import { buildDevServer } from './buildDevServer';
+import webpack from 'webpack'
+import { BuildOptions } from './types/config'
+import { buildPlugins } from './buildPlugins'
+import { buildLoaders } from './buildLoaders'
+import { buildResolvers } from './buildResolvers'
+import { buildDevServer } from './buildDevServer'
 
-export function buildWebpackConfig(options: BuildOptions): webpack.Configuration {
-    const {
-        paths, mode, isDev,
-    } = options;
+export function buildWebpackConfig(
+    options: BuildOptions,
+): webpack.Configuration {
+    const { paths, mode, isDev } = options
 
     return {
         mode,
@@ -24,7 +24,7 @@ export function buildWebpackConfig(options: BuildOptions): webpack.Configuration
             rules: buildLoaders(options),
         },
         resolve: buildResolvers(options),
-        devtool: isDev ? 'eval-cheap-module-source-map' : undefined,
+        // devtool: isDev ? 'eval-cheap-module-source-map' : undefined,
         devServer: isDev ? buildDevServer(options) : undefined,
-    };
+    }
 }
