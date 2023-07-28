1605d1fd7ef2d98273e35c1e3dc51aae4d961800
stonek <amit-yurist@mail.ru>
2023-07-28 11:47:34 +0300

    [fix] try some fixe

--- src/features/SpinnedCube/ui/SpinnedCube/SpinnedCube.tsx
+++ src/features/SpinnedCube/ui/SpinnedCube/SpinnedCube.tsx
@@ -1,34 +1,32 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-    faAngular,
-    faCss3,
-    faGitAlt,
-    faHtml5,
-    faJsSquare,
-    faReact,
-} from '@fortawesome/free-brands-svg-icons'
 import cls from './SpinnedCube.module.scss'
+import Angular from '@/shared/assets/icons/angular.svg'
+import Css3 from '@/shared/assets/icons/css3.svg'
+import ReactIcon from '@/shared/assets/icons/react.svg'
+import GitIcon from '@/shared/assets/icons/git-alt.svg'
+import Html5Icon from '@/shared/assets/icons/html5.svg'
+import JSIcon from '@/shared/assets/icons/js-icon.svg'
+import { Icon } from '@/shared/ui/redesigned/Icon'
 
 export const SpinnedCube = () => (
     <div className={cls['stage-cube-cont']}>
         <div className={cls.cubespinner}>
             <div className={cls.face1}>
-                <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+                <Icon Svg={Angular} width={100} height={100} fill="#DD0031" />
             </div>
             <div className={cls.face2}>
-                <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                <Icon Svg={Html5Icon} width={100} height={100} fill="#F06529" />
             </div>
             <div className={cls.face3}>
-                <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                <Icon Svg={Css3} width={100} height={100} fill="#28A4D9" />
             </div>
             <div className={cls.face4}>
-                <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                <Icon Svg={ReactIcon} width={100} height={100} fill="#5ED4F4" />
             </div>
             <div className={cls.face5}>
-                <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                <Icon Svg={JSIcon} width={100} height={100} fill="#EFD81D" />
             </div>
             <div className={cls.face6}>
-                <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                <Icon Svg={GitIcon} width={100} height={100} fill="#EC4D28" />
             </div>
         </div>
     </div>
