afd4fa8fa16da2b18c54b8403f181a7d521951b6
stonek <amit-yurist@mail.ru>
2023-07-28 14:33:51 +0300

    [fix] try some fixe

--- config/build/loaders/buildCssLoader.ts
+++ config/build/loaders/buildCssLoader.ts
@@ -1,4 +1,4 @@
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin'
 
 export function buildCssLoader(isDev: boolean) {
     return {
@@ -10,7 +10,8 @@ export function buildCssLoader(isDev: boolean) {
                 loader: 'css-loader',
                 options: {
                     modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+                        auto: (resPath: string) =>
+                            Boolean(resPath.includes('.module.')),
                         localIdentName: isDev
                             ? '[path][name]__[local]--[hash:base64:5]'
                             : '[hash:base64:8]',
@@ -19,5 +20,5 @@ export function buildCssLoader(isDev: boolean) {
             },
             'sass-loader',
         ],
-    };
+    }
 }
